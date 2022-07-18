1b7f4fd1588844019b8261dc959a6d266e961d24
Jiho Shin <ziho@kakao.com>
2022-07-18 20:34:53 +0900

    init : express 초기 설정

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import dotenv from 'dotenv'
+import { webpack } from 'webpack'
+import webpackConfig from '../webpack.config'
+
+const compiler = webpack(webpackConfig)
+
+dotenv.config()
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+if (process.env.NODE_ENV === 'development') {
+  // 웹팩 설정
+  app.use(
+    '/dist',
+    middleware(compiler, {
+      // webpack-dev-middleware options
+    }),
+  )
+}
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
