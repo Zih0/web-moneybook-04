36ecb37bd1917849258b9990284ea468bb0a2d7f
minsu-zip <alstn4814@gmail.com>
2022-07-18 22:28:37 +0900

    chore : 핸들러 위치 변경

--- src/backend/app.js
+++ src/backend/app.js
@@ -35,17 +35,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, '../../public')))
 }
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
@@ -55,6 +44,17 @@ app.get('/', (req, res) => {
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
 })
 
 export default app
