1dbb4a3279be0efad942c337035ebb968bcad2d8
Jiho Shin <ziho@kakao.com>
2022-07-19 12:35:26 +0900

    feat : webpack 모드별 config파일 연결

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,11 +5,15 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import dotenv from 'dotenv'
 import webpack from 'webpack'
-import webpackConfig from '../../webpack.config.js'
+import webpackDevConfig from '../../webpack.dev.js'
+import webpackProdConfig from '../../webpack.prod.js'
+
 import Middleware from 'webpack-dev-middleware'
 const __dirname = path.resolve()
 
-const compiler = webpack(webpackConfig)
+const compiler = webpack(
+  process.env.NODE_ENV === 'development' ? webpackDevConfig : webpackProdConfig,
+)
 
 dotenv.config()
 const app = express()
@@ -19,17 +23,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
-if (process.env.NODE_ENV === 'development') {
-  // 웹팩 설정
-  app.use(
-    '/dist',
-    Middleware(compiler, {
-      // webpack-dev-middleware options
-    }),
-  )
-}
+app.use(
+  '/dist',
+  Middleware(compiler, {
+    // webpack-dev-middleware options
+  }),
+)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, '../../public')))
@@ -39,22 +38,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, `../../public/index.html`))
-})
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
 })
 
 export default app
