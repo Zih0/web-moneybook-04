f7bb7cb4edcf84e98591c25eb302c6590ed87ec5
Jiho Shin <ziho@kakao.com>
2022-07-21 13:21:42 +0900

    refactor : 거래내역 분류 한영 매핑 객체 상수로 따로 관리

--- src/frontend/components/TransactionItem/TransactionItem.js
+++ src/frontend/components/TransactionItem/TransactionItem.js
@@ -1,19 +1,7 @@
 import { Component } from '../../core/component.js'
+import { CATEGORY } from '../../utils/constants.js'
 import { priceToString } from '../../utils/stringUtil.js'
 import './transactionItem.scss'
-
-const CATEGORY_MAP = {
-  health: '의료/건강',
-  shopping: '쇼핑/뷰티',
-  traffic: '교통',
-  food: '식비',
-  culture: '문화/여가',
-  undefined: '미분류',
-  life: '생활',
-  salary: '월급',
-  allowance: '용돈',
-  etc: '기타수업',
-}
 
 export default class TransactionItem extends Component {
   template() {
@@ -21,11 +9,10 @@ export default class TransactionItem extends Component {
 
     return /*html*/ `
     <div class="transaction-item">
-      <div class="transaction-item-category ${category}">${CATEGORY_MAP[category]}</div>
+      <div class="transaction-item-category ${category}">${CATEGORY[category]}</div>
       <p class="transaction-item-title">${title}</p>
       <p class="transaction-item-payment">${payment}</p>
       <p class="transaction-item-price">${priceToString(price)}원</p>
-      
     </div>
     `
   }
