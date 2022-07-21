16b9f83ac9cf4007b156ab18ce3824e043351747
minsu-zip <alstn4814@gmail.com>
2022-07-21 17:19:50 +0900

    chore : 서버 라우팅 처리
    와일드카드 라우팅으로 모든 url에 대해 index.html 반환

--- src/backend/app.js
+++ src/backend/app.js
@@ -31,11 +31,10 @@ app.use(
   }),
 )
 
-app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/transaction', transactionRouter)
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, `../../public/index.html`))
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
 })
 
 export default app
