f01ee5b0311a5364e720f382c2f0af7733db3102
minsu-zip <alstn4814@gmail.com>
2022-07-21 17:24:01 +0900

    feat : 404 페이지

---
+++ src/frontend/pages/ErrorPage.js
@@ -0,0 +1,11 @@
+import { Component } from '../core/component.js'
+
+export default class ErrorPage extends Component {
+  template() {
+    return /*html*/ `
+    <div>
+       404 페이지
+    </div>
+    `
+  }
+}
