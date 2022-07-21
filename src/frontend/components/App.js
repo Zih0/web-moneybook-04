07ab9e9542c0c2ef92fdd81545ad0d1d83c8f3c0
minsu-zip <alstn4814@gmail.com>
2022-07-21 18:34:28 +0900

    refactor : 라우팅 경로 상수 사용

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,6 +1,7 @@
 import { Component } from '../core/component.js'
 import Header from './Header/Header.js'
 import ErrorPage from '../pages/ErrorPage.js'
+import { ROUTE } from '../utils/constants.js'
 
 export default class App extends Component {
   constructor($target, props) {
@@ -13,17 +14,15 @@ export default class App extends Component {
   template() {
     const { pathname } = location
     let route = false
-    if (pathname === '/' || pathname === '/calendar' || pathname === '/chart') {
+    if (pathname === ROUTE.fileText || pathname === ROUTE.calendar || pathname === ROUTE.chart) {
       route = true
     }
 
     if (route) {
       return /*html*/ `
             <header>
-
             </header>
             <div class='main'>
-
             </div>
         `
     } else {
@@ -50,12 +49,15 @@ export default class App extends Component {
     // URL에 따른 페이지 라우팅 처리
     const $main = this.$target.querySelector('.main')
     const { pathname } = location
-    if (pathname === '/') {
+    if (pathname === ROUTE.fileText) {
       // 가계부 페이지
-    } else if (pathname === '/calendar') {
+      $main.innerHTML = '메인'
+    } else if (pathname === ROUTE.calendar) {
       // 달력 페이지
-    } else if (pathname === '/chart') {
+      $main.innerHTML = '달력'
+    } else if (pathname === ROUTE.chart) {
       // 차트
+      $main.innerHTML = '차트'
     } else {
       // 404 페이지
       new ErrorPage($main)
