c6998bc81a6e8441ae8a5a888319900b4ff47db5
minsu-zip <alstn4814@gmail.com>
2022-07-22 13:56:55 +0900

    feat : Header 컴포넌트 view 로직

--- src/frontend/components/Header/Header.js
+++ src/frontend/components/Header/Header.js
@@ -1,8 +1,11 @@
 import { Component } from '../../core/component.js'
+import { ROUTE } from '../../utils/constants.js'
+import './header.scss'
 import fileText from '../../assets/file-text.svg'
 import calendar from '../../assets/calendar.svg'
 import chart from '../../assets/chart.svg'
-import { ROUTE } from '../../utils/constants.js'
+import leftArrow from '../../assets/left-arrow.svg'
+import rightArrow from '../../assets/right-arrow.svg'
 
 export default class Header extends Component {
   hadnleRoute(e) {
@@ -16,27 +19,47 @@ export default class Header extends Component {
 
   setEvent() {
     this.dom.querySelector('.route').addEventListener('click', this.hadnleRoute.bind(this))
+    this.dom.querySelector('.logo').addEventListener('click', this.hadnleRoute.bind(this))
   }
 
   template() {
     return /*html*/ `
-    <div class='route'>
-        <div>
-            <button class='fileText' style = "width:100px;">
-                ${fileText}
-            </button>
+    <header>
+      <div class='header-container'>
+        <div class='logo'>
+          <button class='fileText'>우아한 가계부</button>
         </div>
-        <div>
-            <button class='calendar' style = "width:100px;">
-                ${calendar}
-            </button>
+
+        <div class='date-wrap'>
+          <button class="arrow">
+              ${leftArrow}
+          </button>
+
+          <div class='text-wrap'>
+            <span class='month-text'>7월</span>
+            <span class='year-text'>2021</span>
+          </div>
+
+          <button class="arrow">
+              ${rightArrow}
+          </button>
         </div>
-        <div>
-            <button class='chart' style = "width:100px;">
-                ${chart}
-            </button>
+
+        <div class='menu-wrap route'>
+          <button class="fileText">
+            ${fileText}
+          </button>
+
+          <button class="calendar">
+            ${calendar}
+          </button>
+
+          <button class="chart">
+            ${chart}
+          </button>
         </div>
-    </div>
+      </div>
+    </header>
     `
   }
 }
