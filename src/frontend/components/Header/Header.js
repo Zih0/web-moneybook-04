420c59fe9f9342e7d54d3b6c370cb37bb670a0f2
minsu-zip <alstn4814@gmail.com>
2022-07-24 15:08:04 +0900

    feat : 년도,월별 변경 이벤트 및 옵저버 패턴을 활용한 전역 관리

--- src/frontend/components/Header/Header.js
+++ src/frontend/components/Header/Header.js
@@ -1,28 +1,24 @@
 import { Component } from '../../core/component.js'
 import { ROUTE } from '../../utils/constants.js'
-import './header.scss'
 import IconFileText from '../../assets/file-text.svg'
 import IconCalendar from '../../assets/calendar.svg'
 import IconChart from '../../assets/chart.svg'
 import IconLeftArrow from '../../assets/left-arrow.svg'
 import IconRightArrow from '../../assets/right-arrow.svg'
+import { getState, subscribe, setState } from '../../core/observer.js'
+import { dateState } from '../../stores/dateStore.js'
+import './header.scss'
 
 export default class Header extends Component {
-  hadnleRoute(e) {
-    const target = e.target.closest('button')
-    if (!target) return
-
-    const { className } = target
-    history.pushState(null, null, ROUTE[className])
-    this.props.route()
-  }
-
-  setEvent() {
-    this.querySelector('.route').addEventListener('click', this.hadnleRoute.bind(this))
-    this.querySelector('.logo').addEventListener('click', this.hadnleRoute.bind(this))
+  constructor(props) {
+    super(props)
+    subscribe(dateState, this.render.bind(this))
+    this.setDate = setState(dateState)
   }
 
   template() {
+    const { year, month } = getState(dateState)
+
     return /*html*/ `
     <header>
       <div class='header-container'>
@@ -31,16 +27,16 @@ export default class Header extends Component {
         </div>
 
         <div class='date-wrap'>
-          <button class="arrow">
+          <button class="left-arrow">
               ${IconLeftArrow}
           </button>
 
           <div class='text-wrap'>
-            <span class='month-text'>7월</span>
-            <span class='year-text'>2021</span>
+            <span class='month-text'>${month}월</span>
+            <span class='year-text'>${year}</span>
           </div>
 
-          <button class="arrow">
+          <button class="right-arrow">
               ${IconRightArrow}
           </button>
         </div>
@@ -61,6 +57,47 @@ export default class Header extends Component {
       </div>
     </header>
     `
+  }
+
+  setEvent() {
+    this.querySelector('.route').addEventListener('click', this.hadnleRoute.bind(this))
+    this.querySelector('.logo').addEventListener('click', this.hadnleRoute.bind(this))
+    this.querySelector('.date-wrap').addEventListener('click', this.handleDate.bind(this))
+  }
+
+  hadnleRoute(e) {
+    const target = e.target.closest('button')
+    if (!target) return
+
+    const { className } = target
+    history.pushState(null, null, ROUTE[className])
+    this.props.route()
+  }
+
+  handleDate(e) {
+    const $button = e.target.closest('button')
+    if (!$button) return
+
+    const { className } = $button
+    const { year, month } = getState(dateState)
+    const { nextYear, nextMonth } = this.makeCalendar(year, month, className)
+
+    this.setDate({ year: nextYear, month: nextMonth })
+  }
+
+  makeCalendar(year, month, className) {
+    let nextYear = year
+    let nextMonth = className === 'left-arrow' ? month - 1 : month + 1
+
+    if (nextMonth > 12) {
+      nextMonth = 1
+      nextYear += 1
+    } else if (nextMonth < 1) {
+      nextMonth = 12
+      nextYear -= 1
+    }
+
+    return { nextYear, nextMonth }
   }
 }
 
