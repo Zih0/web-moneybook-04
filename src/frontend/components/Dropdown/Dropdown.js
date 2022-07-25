29c89f6f60bbfdc08bf8d3557f0959c05e4c0ae4
minsu-zip <alstn4814@gmail.com>
2022-07-26 00:30:25 +0900

    feat : 수입 목록, 지출 목록 li list추가

--- src/frontend/components/Dropdown/Dropdown.js
+++ src/frontend/components/Dropdown/Dropdown.js
@@ -1,4 +1,5 @@
 import { Component } from '../../core/component.js'
+import { INCOME, EXPENSE } from '../../utils/constants.js'
 import './dropdown.scss'
 
 export default class Dropdown extends Component {
@@ -6,11 +7,13 @@ export default class Dropdown extends Component {
     return /*html*/ `
       <ul class="dropdown-ul">
         <div class="dropdown-title expense">지출</div>
-        <li class="dropdown-li">생활</li>
+        ${Object.keys(EXPENSE)
+          .map((key) => ` <li class="dropdown-li" id=${key}>${EXPENSE[key]}</li>`)
+          .join('')}
         <div class="dropdown-title income">수입</div>
-        <li class="dropdown-li">용돈</li>
-        <li class="dropdown-li">월급</li>
-        <li class="dropdown-li">기타수입</li>
+        ${Object.keys(INCOME)
+          .map((key) => ` <li class="dropdown-li" id=${key}>${INCOME[key]}</li>`)
+          .join('')}
       </ul>
     `
   }
