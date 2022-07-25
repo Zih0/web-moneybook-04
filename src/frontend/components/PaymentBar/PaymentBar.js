eabbbd2d109126c1764895d20f0f6416dfa9090b
minsu-zip <alstn4814@gmail.com>
2022-07-26 01:04:47 +0900

    feat : 분류 상태 값에 따른 state 저장

--- src/frontend/components/PaymentBar/PaymentBar.js
+++ src/frontend/components/PaymentBar/PaymentBar.js
@@ -5,26 +5,38 @@ import plus from '../../assets/plus.svg'
 import Dropdown from '../Dropdown/Dropdown.js'
 
 export default class PaymentBar extends Component {
+  initState() {
+    this.state = {
+      date: '',
+      category: '선택하세요',
+      title: '',
+      payment: '',
+      price: '',
+    }
+  }
+
   template() {
+    const { date, category, title, payment, price } = this.state
+
     return /*html*/ `
     <div class='paymentbar-container'>
         <form class='form-wrapper'>
             <div class='form-element'>
                 <span class='form-element-title'>일자</span>
-                <input class='form-element-input' placeholder='yyyymmdd'/>
+                <input class='form-element-input' placeholder='yyyymmdd' value=${date}>
             </div>
             <div class='form-element'>
                 <span class='form-element-title'>분류</span>
             
                 <div class="form-element-dropdown category">
-                    <div class="category-select">선택하세요</div>
+                    <div class="category-select">${category}</div>
                     <div class="category-dropdown-replace"></div>
                 </div>
 
             </div>
             <div class='form-element'>
                 <span class='form-element-title'>내용</span>
-                <input class='form-element-input' placeholder='입력하세요'/>
+                <input class='form-element-input' placeholder='입력하세요' value=${title}>
             </div>
             <div class='form-element'>
                 <span class='form-element-title'>결제수단</span>
@@ -36,7 +48,7 @@ export default class PaymentBar extends Component {
                 <span class='form-element-title'>금액</span> 
                 <div class='price-input-wrapper'>
                         ${minus}
-                        <input class='form-element-input' placeholder='입력하세요'/>
+                        <input class='form-element-input' placeholder='입력하세요' value=${price}>
                         <span>원</span>
                     </div>
                 </div>
@@ -48,15 +60,24 @@ export default class PaymentBar extends Component {
     `
   }
 
-  handleClickCategorySelect(e) {
+  handleClickCategorySelect() {
     const $dropdown = this.querySelector('.category .dropdown-ul')
-
     $dropdown.classList.toggle('active')
+  }
+
+  handleClickCategoryItem(e) {
+    this.setState({
+      category: e.target.innerText,
+    })
+    this.handleClickCategorySelect.bind(this)
   }
 
   setEvent() {
     const $categorySelect = this.querySelector('.category-select')
     $categorySelect.addEventListener('click', this.handleClickCategorySelect.bind(this))
+
+    const $categoryItem = this.querySelector('.dropdown-ul')
+    $categoryItem.addEventListener('click', this.handleClickCategoryItem.bind(this))
   }
 
   setComponent() {
