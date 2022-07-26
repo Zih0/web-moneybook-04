f8a39979cd886e6916b4a52045e9784e6dbcefef
Jiho Shin <ziho@kakao.com>
2022-07-26 17:08:16 +0900

    feat : 해당 카테고리 6개월 지출 거래내역 라우트 연결

--- src/backend/routes/transactionHistory.js
+++ src/backend/routes/transactionHistory.js
@@ -2,11 +2,14 @@ import express from 'express'
 import {
   getExpenseTransactionHistory,
   getTransactionHistory,
+  getSixMonthCategoryExpenseTransactionHistory,
 } from '../controllers/transactionHistory.js'
 
 const transactionRouter = express.Router()
 
 transactionRouter.get('/', getTransactionHistory)
 transactionRouter.get('/category', getExpenseTransactionHistory)
+
+transactionRouter.get('/category/six-month', getSixMonthCategoryExpenseTransactionHistory)
 
 export { transactionRouter }
