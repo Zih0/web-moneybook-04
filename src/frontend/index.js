22be1db479092017e92357d4c3b1feaf3a0635b5
Jiho Shin <ziho@kakao.com>
2022-07-27 12:57:53 +0900

    feat : 결제수단 생성, 삭제 모달 modal wrapper에 추가

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,4 +1,9 @@
 import './styles/style.scss'
 import App from './components/App.js'
+import AddPaymentModal from './components/Modal/AddPaymentModal.js'
+import RemovePaymentModal from './components/Modal/RemovePaymentModal.js'
 
 document.querySelector('#app').appendChild(new App())
+
+document.querySelector('#modal').appendChild(new AddPaymentModal())
+document.querySelector('#modal').appendChild(new RemovePaymentModal())
