acbbfd25259d58356e8f96c5d4673b534e1c1e0a
minsu-zip <alstn4814@gmail.com>
2022-07-27 16:43:24 +0900

    feat : 현재 날짜 구하기

--- src/frontend/utils/stringUtil.js
+++ src/frontend/utils/stringUtil.js
@@ -2,4 +2,21 @@ const priceToString = (price) => {
   return Number(price).toLocaleString()
 }
 
-export { priceToString }
+const todayDate = () => {
+  const leftPad = (value) => {
+    if (value >= 10) {
+      return value
+    }
+
+    return `0${value}`
+  }
+
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = leftPad(date.getMonth() + 1)
+  const day = leftPad(date.getDate())
+
+  return [year, month, day].join('-')
+}
+
+export { priceToString, todayDate }
