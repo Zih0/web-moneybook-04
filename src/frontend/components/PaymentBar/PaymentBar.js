af5aa2ae04ed2c72b2cf32c4c8a38e44ba6fbb19
minsu-zip <alstn4814@gmail.com>
2022-07-27 16:44:04 +0900

    feat : 결제수단 create 로직 개발

--- src/frontend/components/PaymentBar/PaymentBar.js
+++ src/frontend/components/PaymentBar/PaymentBar.js
@@ -5,25 +5,28 @@ import minus from '../../assets/minus.svg'
 import plus from '../../assets/plus.svg'
 import CategoryDropdown from '../CategoryDropdown/CategoryDropdown.js'
 import PaymentDropdown from '../PaymentDropdown/PaymentDropdown.js'
+import { todayDate } from '../../utils/stringUtil.js'
+import { createTransactionAPI } from '../../api/transactionHistory.js'
 
 export default class PaymentBar extends Component {
   initState() {
     this.state = {
-      paymentDate: '',
+      paymentDate: todayDate(),
       category: '',
       title: '',
       payment_id: 0,
       price: 0,
       option: false,
+      disabled: true,
     }
   }
 
   template() {
-    const { paymentDate, category, title, payment_id, price, option } = this.state
+    const { paymentDate, category, title, payment_id, price, option, disabled } = this.state
 
     return /*html*/ `
     <div class='paymentbar-container'>
-        <form class='form-wrapper'>
+        <div class='form-wrapper'>
             <div class='form-element'>
                 <span class='form-element-title'>일자</span>
                 <input class='form-element-input' id='paymentDate' placeholder='yyyymmdd' maxlength='10' value=${paymentDate}>
@@ -61,22 +64,27 @@ export default class PaymentBar extends Component {
                         <span>원</span>
                     </div>
                 </div>
-            <button class='form-button' disabled>
-            ${plus}
+            <button class='form-button'>
+              ${plus}
             </button>
-        </form>
+        </div>
     </div>
     `
   }
 
   setEvent() {
     // 각 입력 필드별로 다른 예외 처리를 해야하므로 각각의 이벤트 할당
-
     const $paymentDate = this.querySelector('#paymentDate')
-    $paymentDate.addEventListener('keyup', this.handleInputPaymentDate.bind(this))
-    $paymentDate.addEventListener('change', this.handleInputPaymentDateState.bind(this))
+    $paymentDate.addEventListener('input', this.PaymentDataRegExp.bind(this))
+    $paymentDate.addEventListener('change', this.handleInputPaymentDate.bind(this))
 
     this.querySelector('#title').addEventListener('change', this.handleInputTitle.bind(this))
+
+    const $price = this.querySelector('#price')
+    $price.addEventListener('input', this.PriceRegExp.bind(this))
+    $price.addEventListener('change', this.handleInputPrice.bind(this))
+
+    this.querySelector('.form-button').addEventListener('click', this.submitForm.bind(this))
 
     const $categorySelectList = this.querySelectorAll('.select-dropdown')
     $categorySelectList.forEach((item) =>
@@ -96,6 +104,23 @@ export default class PaymentBar extends Component {
     )
   }
 
+  submitForm(e) {
+    const result = e.target.closest('.submit')
+    if (!result) return
+
+    const { paymentDate, category, title, payment_id, price, option } = this.state
+    let tmpPrice = Number(price.replace(/,/g, ''))
+    tmpPrice = option ? tmpPrice : -tmpPrice
+    const data = {
+      paymentDate,
+      category,
+      title,
+      payment_id,
+      price: tmpPrice,
+    }
+    createTransactionAPI(data)
+  }
+
   handleClickCategorySelect(e) {
     const { id } = e.target
     if (id) {
@@ -104,8 +129,22 @@ export default class PaymentBar extends Component {
     }
   }
 
+  checkFormButton() {
+    const check = Object.values(this.state).every((state) => state !== '' && state !== 0)
+
+    this.setState({
+      disabled: !check,
+    })
+
+    if (check) {
+      this.querySelector('.form-button').classList.add('submit')
+    } else {
+      this.querySelector('.form-button').classList.remove('submit')
+    }
+  }
+
   // 날짜 입력 값 정규 표현식
-  handleInputPaymentDate(e) {
+  PaymentDataRegExp(e) {
     e.target.value = e.target.value
       .replace(/[^0-9]/g, '')
       .replace(/^(\d{0,4})(\d{0,2})(\d{0,2})$/g, '$1-$2-$3')
@@ -113,13 +152,19 @@ export default class PaymentBar extends Component {
       .replace()
   }
 
-  // 필드 입력 관련 함수
+  // 가격 입력 정규 표현식
+  PriceRegExp(e) {
+    e.target.value = Number(e.target.value.replace(/[^0-9]/g, '')).toLocaleString()
+  }
+
+  /* 필드 입력 관련 함수 */
 
   // 날짜 입력 폼
-  handleInputPaymentDateState(e) {
+  handleInputPaymentDate(e) {
     this.setState({
       paymentDate: e.target.value,
     })
+    this.checkFormButton()
   }
 
   // 카테고리 입력 폼
@@ -128,6 +173,7 @@ export default class PaymentBar extends Component {
       category: selectedItem,
       option: option,
     })
+    this.checkFormButton()
   }
 
   // 내용 입력 폼
@@ -135,6 +181,7 @@ export default class PaymentBar extends Component {
     this.setState({
       title: e.target.value,
     })
+    this.checkFormButton()
   }
 
   // 결제수단 입력 폼
@@ -142,12 +189,15 @@ export default class PaymentBar extends Component {
     this.setState({
       payment_id: 2,
     })
+    this.checkFormButton()
   }
 
+  // 가격 입력 폼
   handleInputPrice(e) {
     this.setState({
-      title: e.target.value,
+      price: e.target.value,
     })
+    this.checkFormButton()
   }
 }
 
