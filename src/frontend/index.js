2781486739756d2baaa56a4f89824dd4195d97e4
Jiho Shin <ziho@kakao.com>
2022-07-27 15:57:06 +0900

    feat : index.js에서 모달 넣는 방식 제거

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,9 +1,4 @@
 import './styles/style.scss'
 import App from './components/App.js'
-import AddPaymentModal from './components/Modal/AddPaymentModal.js'
-import RemovePaymentModal from './components/Modal/RemovePaymentModal.js'
 
 document.querySelector('#app').appendChild(new App())
-
-// document.querySelector('#modal').appendChild(new AddPaymentModal())
-document.querySelector('#modal').appendChild(new RemovePaymentModal())
